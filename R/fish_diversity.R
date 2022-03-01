d7227cad7c807856e81791ebcca8c5c9e21ace5d
Genevieve <genevieve@Genevieves-MacBook-Pro.local>
2022-03-01 13:17:21 -0800

    edited function names

--- R/fish_diversity.R
+++ R/fish_diversity.R
@@ -6,7 +6,7 @@
 #' \describe{
 #' \item{common}{Most common fish}
 #' \item{rarest}{Rarest fish}
-#' \item{number}{Total number of fish}
+#' \item{count}{Total number of fish}
 #' }
 #' @examples 
 #' fish_diversity(c("salmon", "tuna", "steelhead", "cod", "shark", "salmon", "cod"))
@@ -14,24 +14,19 @@
 # function definition
 fish_diversity = function(fish_species) {
   
-  fish_species = as.factor(fish_species)
+  fish_species = as.factor(fish_species$x)
   
   # most common fish
-  common_r = which.max(summary(fish_species))
-  common = names(summary(fish_species)[common_r])
-  
-  #common = names(which.max(summary(fish_species)))
+  common = names(which.max(summary(fish_species)))
   
   # rarest fish
-  rarest_r = which.min(summary(fish_species))
-  rarest = names(summary(fish_species)[rarest_r])
-  #rarest = names(which.min(summary(fish_species)))
+  rarest = names(which.min(summary(fish_species)))
   
   # total number of fish
-  number = length(fish_species)
+  count = length(fish_species)
   
   # output from function
-  return(list(common = common, rarest = rarest, number = number))
+  return(list(common = common, rarest = rarest, count = count))
 }
 
 
